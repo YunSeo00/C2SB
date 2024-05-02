76b71ad33dd6df52d761460dcce6dffa557f4b94
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2024-05-02 15:23:47 +0900

    [Feat] Add ploting reward results

--- utils/plot.py
+++ utils/plot.py
@@ -9,9 +9,9 @@ def read_options(args=sys.argv[1:]):
     # simulation setting
     parser.add_argument('--dataset', help='The dataset to use', type=str, default='numerical')
     parser.add_argument('--iterations', help='The number of iterations', type=int, default=5)
-    parser.add_argument('--tuning_time_horizon', help='The length of tuning rounds', type=int, default=100)
+    parser.add_argument('--tuning_time_horizon', help='The length of tuning rounds', type=int, default=300)
     parser.add_argument('--time_horizon', help='The length of rounds', type=int, default=10000)
-    parser.add_argument('--error_var', help='The variance of the error term', type=float, default=1)
+    parser.add_argument('--error_var', help='The variance of the error term', type=float, default=0.1)
     parser.add_argument('--dim', help='The dimension of the context', type=int, default=2)
     parser.add_argument('--arms', help='The number of arms', type=int, default=10)
     parser.add_argument('--nu', help='The parameter of the time-varying term', type=str, default='set3')
@@ -19,6 +19,7 @@ def read_options(args=sys.argv[1:]):
     parser.add_argument('--super_set_size', help='The size of the super set', type=int, default=4)
     parser.add_argument('--seed', help='The seed for the random number generator', type=int, default=42)
     parser.add_argument('--models', help='The models to graph', type=str, default='["LinUCB", "LinTS", "C2SB"]')
+    parser.add_argument('--which_plot', help='The type of plot to generate', type=str, default='regret') # ['regret', 'reward']
     opts = parser.parse_args(args)
     return opts
 
@@ -40,7 +41,7 @@ models = eval(opts.models)
 colors = ['r','b','g','y','m','c','k','o','p']
 
 for model, oracle in eval(opts.models):
-    data = np.load(f'{save_path}/cum_regret_{model}_{oracle}_{save_name}.npy')
+    data = np.load(f'{save_path}/cum_{opts.which_plot}_{model}_{oracle}_{save_name}.npy')
     
     model_mean = np.mean(data, axis=0)
     model_std = np.std(data, axis=0)
