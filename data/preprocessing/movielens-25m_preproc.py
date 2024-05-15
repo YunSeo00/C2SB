00f4d3a4928ac4fb47e57c1771ddb3fb61d060cf
YunSeo00 <chldbstj00@sookmyung.ac.kr>
2024-05-16 00:58:51 +0900

    [Feat] Add reindexing step

--- data/preprocessing/movielens-25m_preproc.py
+++ data/preprocessing/movielens-25m_preproc.py
@@ -4,29 +4,36 @@ from pmf import PMF
 
 np.random.seed(42)
 
-ratings = pd.read_csv(filepath_or_buffer='../ml-25m/raw_data/ratings.csv')
+ratings = pd.read_csv(filepath_or_buffer='./data/ml-25m/raw_data/ratings.csv')
 ratings = ratings.drop('timestamp', axis=1)
 
 # Filter out users with less than 100 ratings
 user_rating_counts = ratings.groupby('userId').size()
-qualified_movies = user_rating_counts[user_rating_counts >= 100].index
-ratings = ratings[ratings['userId'].isin(qualified_movies)]
+qualified_users = user_rating_counts[user_rating_counts >= 100].index
+ratings = ratings[ratings['userId'].isin(qualified_users)]
 
 # Split data into train and test sets
-train_user = np.random.choice(ratings['userId'].unique(), 6000, replace=False)
-train = ratings[ratings['userId'].isin(train_user)]
-test = ratings[~ratings['userId'].isin(train_user)]
-test = test[test['movieId'].isin(train['movieId'])] # Filter out movies not in train set
+train_users = np.random.choice(ratings['userId'].unique(), 6000, replace=False)
+train = ratings[ratings['userId'].isin(train_users)]
+test = ratings[~ratings['userId'].isin(train_users)]
+test = test[test['movieId'].isin(train['movieId'])]  # Filter out movies not in train set
 
-train = train.pivot(index='userId', columns='movieId', values='rating').fillna(0).values
+# Reindex movieId from 0 to num_movies - 1
+item_id_index = {movie_id: i for i, movie_id in enumerate(train['movieId'].unique())}
+train['movieId'] = train['movieId'].map(item_id_index)
+test['movieId'] = test['movieId'].map(item_id_index)
+
+# Pivot train data for PMF model
+train_matrix = train.pivot(index='userId', columns='movieId', values='rating').fillna(0).values
 
 # Train PMF model
-pmf = PMF(num_factors=10, learning_rate=0.001, num_epochs=100, patience=20, batch_size=65536)
-pmf.fit(train)
+pmf = PMF(num_factors=10, learning_rate=0.1, num_epochs=1, patience=20, batch_size=65536)
+pmf.fit(train_matrix)
 
-# save movie embedding and test set
-np.save('../ml-25m/preproc_data/item_emb.npy', pmf.V) # ith row is the embedding for movie i
-test.to_csv('../ml-25m/preproc_data/test.csv', index=False)
+print(pmf.V.shape)
+# Save movie embeddings and test set
+np.save('./data/ml-25m/preproc_data/item_emb.npy', pmf.V)  # ith row is the embedding for movie i
+test.to_csv('./data/ml-25m/preproc_data/test.csv', index=False)
 
-# print the top 10 movies embedding
+# Print the top 10 movie embeddings
 print(pmf.V[:10])
